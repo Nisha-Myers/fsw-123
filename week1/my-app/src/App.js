34dc38d452aa470d3a8022d6d775d27ac28d8643
unknown <equiliberalium89@gmail.com>
2022-07-21 20:12:18 -0400

    .

---
+++ week1/my-app/src/App.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+import {v4 as uuidv4} from 'uuid'; 
+import {storeTodos} from './components/store';
+
+import TodoList from './components/TodoList';
+import TodoForm from './components/todoForm';
+import './App.css';
+
+
+
+function App(){
+
+  const [toDos, setToDos] = useState(storeTodos)
+  const toDoArray = [...toDos];
+
+  const completeTodo = (id) => {
+    const index = toDos.findIndex(toDo => toDo.id === id)
+    toDoArray[index].isComplete = !toDoArray[index].isComplete
+    setToDos(toDoArray)
+  }
+
+  const deleteTodo = (id) => {
+    const newToDos = toDos.filter(toDo => toDo.id !== id)
+    setToDos(newToDos)
+  }
+
+  const addTodo = (text) => {
+    const todoToAdd = {
+      id: uuidv4(),
+      text: text,
+      isComplete: false
+    }
+    toDoArray.push(todoToAdd)
+    setToDos(toDoArray)
+  }
+
+  
+  return (
+    <div className="App">
+      <h1>List of Todos</h1>
+      <TodoForm addTodo={addTodo}/>
+      <TodoList text={toDos} completeTodo={completeTodo} deleteTodo={deleteTodo}/>
+    </div>
+  );
+}
+
+export default App;
